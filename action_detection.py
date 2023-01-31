5de754a17a1987519aee2d972b2e8174f6e71cd4
Akilah Lapure <akilah.lapure@gmail.com>
2023-01-31 22:28:45 +0400

    train model

--- action_detection.py
+++ action_detection.py
@@ -5,6 +5,13 @@ from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
 
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
@@ -38,30 +45,30 @@ def draw_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                             )
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-# Set mediapipe model 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
+# # Set mediapipe model 
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+#     while cap.isOpened():
 
-        # Read feed
-        ret, frame = cap.read()
+#         # Read feed
+#         ret, frame = cap.read()
 
-        # Make detections
-        image, results = mediapipe_detection(frame, holistic)
-        print(results)
+#         # Make detections
+#         image, results = mediapipe_detection(frame, holistic)
+#         print(results)
         
-        # Draw landmarks
-        draw_landmarks(image, results)
+#         # Draw landmarks
+#         draw_landmarks(image, results)
 
-        # Show to screen
-        cv2.imshow('OpenCV Feed', image)
+#         # Show to screen
+#         cv2.imshow('OpenCV Feed', image)
 
-        # Break gracefully
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+#         # Break gracefully
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
@@ -89,51 +96,82 @@ for action in actions:
         except:
             pass
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 # Set mediapipe model 
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
     # NEW LOOP
     # Loop through actions
-    for action in actions:
-        # Loop through sequences aka videos
-        for sequence in range(no_sequences):
-            # Loop through video length aka sequence length
-            for frame_num in range(sequence_length):
+    # for action in actions:
+    #     # Loop through sequences aka videos
+    #     for sequence in range(no_sequences):
+    #         # Loop through video length aka sequence length
+    #         for frame_num in range(sequence_length):
 
-                # Read feed
-                ret, frame = cap.read()
+    #             # Read feed
+    #             ret, frame = cap.read()
 
-                # Make detections
-                image, results = mediapipe_detection(frame, holistic)
-                # print(results)
+    #             # Make detections
+    #             image, results = mediapipe_detection(frame, holistic)
+    #             # print(results)
 
-                # Draw landmarks
-                draw_landmarks(image, results)
+    #             # Draw landmarks
+    #             draw_landmarks(image, results)
                 
-                # NEW Apply wait logic
-                if frame_num == 0: 
-                    cv2.putText(image, 'STARTING COLLECTION', (120,200), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
-                    cv2.waitKey(2000)
-                else: 
-                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-                    # Show to screen
-                    cv2.imshow('OpenCV Feed', image)
+    #             # NEW Apply wait logic
+    #             if frame_num == 0: 
+    #                 cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+    #                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
+    #                 cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+    #                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    #                 # Show to screen
+    #                 cv2.imshow('OpenCV Feed', image)
+    #                 cv2.waitKey(2000)
+    #             else: 
+    #                 cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+    #                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+    #                 # Show to screen
+    #                 cv2.imshow('OpenCV Feed', image)
                 
-                # NEW Export keypoints
-                keypoints = extract_keypoints(results)
-                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
-                np.save(npy_path, keypoints)
+    #             # # NEW Export keypoints
+    #             # keypoints = extract_keypoints(results)
+    #             # npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+    #             # np.save(npy_path, keypoints)
 
-                # Break gracefully
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
+    #             # Break gracefully
+    #             if cv2.waitKey(10) & 0xFF == ord('q'):
+    #                 break
                     
-    cap.release()
-    cv2.destroyAllWindows()
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+label_map = {label:num for num, label in enumerate(actions)}
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
