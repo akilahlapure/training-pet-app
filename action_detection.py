b3bf353864d38f527a951a2bb7dbc991dc90fe7c
Akilah Lapure <akilah.lapure@gmail.com>
2023-02-01 21:21:44 +0400

    add welcome screen frame

--- action_detection.py
+++ action_detection.py
@@ -5,16 +5,15 @@ from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
 
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
+import tkinter as tk
+from PIL import Image, ImageTk
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import load_model
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
+########################################### FUNCTIONS ###########################################
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
     image.flags.writeable = False                  # Image is no longer writeable
@@ -45,31 +44,6 @@ def draw_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                             )
 
-# cap = cv2.VideoCapture(0)
-
-# # Set mediapipe model 
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-#     while cap.isOpened():
-
-#         # Read feed
-#         ret, frame = cap.read()
-
-#         # Make detections
-#         image, results = mediapipe_detection(frame, holistic)
-#         print(results)
-        
-#         # Draw landmarks
-#         draw_landmarks(image, results)
-
-#         # Show to screen
-#         cv2.imshow('OpenCV Feed', image)
-
-#         # Break gracefully
-#         if cv2.waitKey(10) & 0xFF == ord('q'):
-#             break
-#     cap.release()
-#     cv2.destroyAllWindows()
-
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
@@ -77,101 +51,102 @@ def extract_keypoints(results):
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, face, lh, rh])
 
-# Path for exported data, numpy arrays
-DATA_PATH = os.path.join('MP_Data') 
+def open_camera():
+    opencam_text.set("Loading...")
 
-# Actions that we try to detect
-actions = np.array(['sit', 'laydown', 'spin'])
+    # Actions that we try to detect
+    actions = np.array(['sit', 'laydown', 'spin', 'up', 'stay'])
 
-# Thirty videos worth of data
-no_sequences = 30
+    # Load training model
+    model = load_model('action.h5')
 
-# Videos are going to be 30 frames in length
-sequence_length = 30
+    # Detection variables
+    sequence = []
+    sentence = []
+    predictions = []
+    threshold = 0.5
 
-for action in actions: 
-    for sequence in range(no_sequences):
-        try: 
-            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
-        except:
-            pass
+    cap = cv2.VideoCapture(0)
 
-# cap = cv2.VideoCapture(0)
-# Set mediapipe model 
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    
-    # NEW LOOP
-    # Loop through actions
-    # for action in actions:
-    #     # Loop through sequences aka videos
-    #     for sequence in range(no_sequences):
-    #         # Loop through video length aka sequence length
-    #         for frame_num in range(sequence_length):
+    # Set mediapipe model 
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
 
-    #             # Read feed
-    #             ret, frame = cap.read()
+            # Read feed
+            ret, frame = cap.read()
 
-    #             # Make detections
-    #             image, results = mediapipe_detection(frame, holistic)
-    #             # print(results)
-
-    #             # Draw landmarks
-    #             draw_landmarks(image, results)
+            # Make detections
+            image, results = mediapipe_detection(frame, holistic)
+            print(results)
+            
+            # Draw landmarks
+            draw_landmarks(image, results)
+            
+            # 2. Prediction logic
+            keypoints = extract_keypoints(results)
+            sequence.append(keypoints)
+            sequence = sequence[-30:]
+            
+            if len(sequence) == 30:
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                print(actions[np.argmax(res)])
+                predictions.append(np.argmax(res))
                 
-    #             # NEW Apply wait logic
-    #             if frame_num == 0: 
-    #                 cv2.putText(image, 'STARTING COLLECTION', (120,200), 
-    #                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-    #                 cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-    #                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-    #                 # Show to screen
-    #                 cv2.imshow('OpenCV Feed', image)
-    #                 cv2.waitKey(2000)
-    #             else: 
-    #                 cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-    #                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-    #                 # Show to screen
-    #                 cv2.imshow('OpenCV Feed', image)
                 
-    #             # # NEW Export keypoints
-    #             # keypoints = extract_keypoints(results)
-    #             # npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
-    #             # np.save(npy_path, keypoints)
+            #3. Viz logic
+                if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                    if res[np.argmax(res)] > threshold: 
+                        
+                        if len(sentence) > 0: 
+                            if actions[np.argmax(res)] != sentence[-1]:
+                                sentence.append(actions[np.argmax(res)])
+                        else:
+                            sentence.append(actions[np.argmax(res)])
 
-    #             # Break gracefully
-    #             if cv2.waitKey(10) & 0xFF == ord('q'):
-    #                 break
-                    
-    # cap.release()
-    # cv2.destroyAllWindows()
+                if len(sentence) > 5: 
+                    sentence = sentence[-5:]
+                
+            cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+            cv2.putText(image, ' '.join(sentence), (3,30), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # Show to screen
+            cv2.imshow('OpenCV Feed', image)
 
-label_map = {label:num for num, label in enumerate(actions)}
+            # Break gracefully
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
 
-sequences, labels = [], []
-for action in actions:
-    for sequence in range(no_sequences):
-        window = []
-        for frame_num in range(sequence_length):
-            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
-            window.append(res)
-        sequences.append(window)
-        labels.append(label_map[action])
+def open_instructions():
+    print("heyyyy")
 
-X = np.array(sequences)
-y = to_categorical(labels).astype(int)
+#################################### START WELCOME SCREEN #################################### 
+root = tk.Tk()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
 
-log_dir = os.path.join('Logs')
-tb_callback = TensorBoard(log_dir=log_dir)
+# Logo
+logo = Image.open('assets/logo_placeholder.jpg')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
 
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
+# Slogan
+slogan = tk.Label(root, text="Train yourself to train your pet!", font="Raleway")
+slogan.grid(columnspan=3, column=0, row=1)
 
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
+# Button
+opencam_text = tk.StringVar()
+opencam_btn = tk.Button(root, textvariable=opencam_text, command=lambda:open_camera(), font="Raleway", bg="#b85c2d", fg="white", height=2, width=15)
+opencam_text.set("Get Started")
+opencam_btn.grid(column=1, row=2)
+
+canvas = tk.Canvas(root, width=600, height=200)
+canvas.grid(columnspan=3)
+
+root.mainloop()
+#################################### END WELCOME SCREEN #################################### 
